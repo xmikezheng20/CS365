97cd446e2a9ec2ebeb3137e216744ce409fa11ea
xzheng902 <xzheng20@colby.edu>
2019-02-16 14:47:46 -0500

    project 2 setup

---
+++ project2/src/cbir.cpp
@@ -0,0 +1,17 @@
+/*
+  cbir.cpp
+  Mike Zheng and Heidi He
+  CS365 project 2
+  2/16/19
+*/
+
+
+#include <cstdio>
+#include <cstring>
+#include "opencv2/opencv.hpp"
+
+int main(int argc, char *argv[]) {
+  printf("Running CBIR\n");
+
+	return(0);
+}
