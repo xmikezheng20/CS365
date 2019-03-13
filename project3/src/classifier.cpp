3507b3b75c8d4cef617da04334bc4715efaa412f
xzheng902 <xzheng20@colby.edu>
2019-03-12 21:57:43 -0400

    Set up classifier test, makefile, and more class structure

--- project3/src/classifier.cpp
+++ project3/src/classifier.cpp
@@ -10,3 +10,37 @@
 
 
 #include "classifier.hpp"
+
+// classifier class
+
+// constructor
+Classifier::Classifier(int type) {
+    printf("constructing\n");
+    this->type = type;
+}
+
+// type setter and getter
+int Classifier::getType() {
+    return this->type;
+}
+void Classifier::setType(int newType) {
+    this->type = newType;
+}
+
+/*compute the confusion matrix*/
+std::vector<std::vector<int>> Classifier::confusion_matrix(
+    std::vector<int> truecats, std::vector<int> classcats) {
+
+    std::vector<std::vector<int>> conf_mat;
+
+
+
+
+
+
+    return conf_mat;
+}
+
+// scaled euclidean classifier
+// constructor
+ScaledEuclidean::ScaledEuclidean():Classifier::Classifier(0){;}
