8f7615b90ccd2934fddeb09477e5081f10e8d66f
xzheng902 <xzheng20@colby.edu>
2019-05-14 17:07:51 -0400

    remove commented code

--- project6/src/metadata_parser.py
+++ project6/src/metadata_parser.py
@@ -66,27 +66,6 @@ def parseXML(xmlfile):
 
     return items
 
-    # # iterate news items
-    # for item in root.findall('./channel/item'):
-    #
-    #     # empty news dictionary
-    #     news = {}
-    #
-    #     # iterate child elements of item
-    #     for child in item:
-    #
-    #         # special checking for namespace object content:media
-    #         if child.tag == '{http://search.yahoo.com/mrss/}content':
-    #             news['media'] = child.attrib['url']
-    #         else:
-    #             news[child.tag] = child.text.encode('utf8')
-    #
-    #     # append news dictionary to news items list
-    #     newsitems.append(news)
-    #
-    # # return news items list
-    # return newsitems
-
 def main(argv):
 
     # usage
